ffa5b703b466b778d20b8e28b8cc2c674bc73329
FrancescoBonzi <fra.bonzi@gmail.com>
2020-10-08 13:40:46 +0200

    bug fix

--- UniboEventClass.js
+++ UniboEventClass.js
@@ -1,8 +1,11 @@
 class UniboEventClass {
-    constructor(title, start, end) {
+    constructor(title, start, end, location, url, docente) {
         this.title = title;
         this.start = start;
         this.end = end;
+        this.location = location;
+        this.url = url;
+        this.organizer = {name: docente, email: "nome.cognome@unibo.it"};
     }
 }
 
