04df96640ba70f94d06842c9310bf7b82368d7c9
FrancescoBonzi <fra.bonzi@gmail.com>
2020-10-08 14:45:08 +0200

    bug fix

--- UniboEventClass.js
+++ UniboEventClass.js
@@ -1,12 +1,19 @@
 class UniboEventClass {
     constructor(title, start, end, location, url, docente) {
         this.title = title;
-        this.start = start;
-        this.end = end;
+        this.start = this.castTimeFromUtcToLocalItaly(start);
+        this.startInputType = "utc";
+        this.end = this.castTimeFromUtcToLocalItaly(end);
+        this.endInputType = "utc";
         this.location = location;
         this.url = url;
         this.organizer = {name: docente, email: "nome.cognome@unibo.it"};
     }
+}
+
+UniboEventClass.prototype.castTimeFromUtcToLocalItaly = function(dateTime) {
+    dateTime[3] -= 2;
+    return dateTime;
 }
 
 module.exports = UniboEventClass;
