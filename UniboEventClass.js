3f156cb55d072165d479986118ab20d7122e0e75
FrancescoBonzi <fra.bonzi@gmail.com>
2020-10-19 18:45:48 +0200

    try to fix hour bug

--- UniboEventClass.js
+++ UniboEventClass.js
@@ -2,11 +2,11 @@ class UniboEventClass {
     constructor(title, start, end, location, url, docente) {
         this.title = title;
         this.start = this.castTimeFromUtcToLocalItaly(start);
-        this.startInputType = "local";
-        this.startOutputType = "local";
+        this.startInputType = "utc";
+        this.startOutputType = "utc";
         this.end = this.castTimeFromUtcToLocalItaly(end);
-        this.endInputType = "utc";
-        this.endOutputType = "utc";
+        this.endInputType = "local";
+        this.endOutputType = "local";
         this.location = location;
         this.url = url;
         this.organizer = {name: docente, email: "nome.cognome@unibo.it"};
