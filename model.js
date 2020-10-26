872a7d46cb5f42c17e1e4de8e7c44c10f05b1845
FrancescoBonzi <fra.bonzi@gmail.com>
2020-10-26 23:16:28 +0100

    several improvements and bug fix

--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
-const csv = require('csv-parser')
-const fs = require('fs')
+const csv = require('csv-parser');
+const fs = require('fs');
 const iCalendar = require('./icalendar');
 const UniboEventClass = require('./UniboEventClass');
 
@@ -14,76 +14,9 @@ const language = {
     "2cycle": "timetable"
 }
 
-var data_file = '';
-
-/*
-function checkIfOpendataFileIsUpToDate(callback) {
-    fetch(root_unibo).then(x => x.text())
-        .then(function (html) {
-            var $ = cheerio.load(html);
-            const relative_path = $('#dataset-resources ul li a').filter('.heading').first().attr('href');
-            console.log('relative_path = ' + relative_path);
-            title = relative_path.split('/')[relative_path.split('/').length - 1];
-            // Check if the file exists in the current directory.
-            const local_path = 'opendata/' + title + '.csv';
-            var up_to_date = false;
-            fs.access(local_path, fs.constants.F_OK, (err) => {
-                if (!err) up_to_date = true;
-                callback(title, up_to_date);
-            });
-        })
-        .catch(function (err) {
-            console.log(err);
-            return;
-        });
-}
-
-function downloadUpToDateOpendataFile(title, callback) {
-    var path_to_download_csv = root_unibo + 'resource/' + title + '/download/' + title;
-    console.log(path_to_download_csv);
-    fetch(path_to_download_csv).then(x => x.text())
-        .then(function (csv) {
-            //Saving file in opendata folder
-            const local_path_of_csv = 'opendata/' + title + '.csv';
-            fs.writeFile(local_path_of_csv, csv, function (err) {
-                if (err) {
-                    console.log(err);
-                    return;
-                };
-                console.log('Saved!');
-                callback(true);
-            });
-        })
-        .catch(function (err) {
-            console.log(err);
-            console.log('Fail to download ' + title + ' from: ' + path);
-            return;
-        });
-}
+var data_file = './opendata/corsi.csv';
 
 function getAreas(callback) {
-    console.log('Entro in getAreas');
-    checkIfOpendataFileIsUpToDate(function (title, up_to_date) {
-        if (!up_to_date) {
-            downloadUpToDateOpendataFile(title, function (downloaded_and_saved) {
-                if (!downloaded_and_saved) {
-                    console.log('Error in downloading and saving of opendata file in opendata folder.');
-                    return;
-                }
-                readCsvAndGetAreas(title, function (results) {
-                    callback(results);
-                });
-            });
-        } else {
-            readCsvAndGetAreas(title, function (results) {
-                callback(results);
-            });
-        }
-    });
-}
-*/
-
-function readCsvAndGetAreas(callback) {
     //Reading csv file and building an array of unique values
     var results = [];
     fs.createReadStream(data_file)
@@ -102,21 +35,6 @@ function readCsvAndGetAreas(callback) {
             areas = Array.from(new Set(areas)).sort();
             callback(areas);
         });
-}
-
-function getAreas(callback) {
-
-    // Finding most recent file of opendata directory
-    const data_dir = './opendata/';
-    fs.readdir(data_dir, function (err, files) {
-        var title = files[0];
-        data_file = data_dir + title;
-
-        // Reading csv file
-        readCsvAndGetAreas(function (results) {
-            callback(results);
-        });
-    });
 }
 
 function getCoursesGivenArea(area, callback) {
@@ -157,6 +75,14 @@ function getTimetableUrlGivenUniboUrl(unibo_url, callback) {
 
 function getCurriculaGivenCourseUrl(unibo_url, callback) {
     getTimetableUrlGivenUniboUrl(unibo_url, function (timetable_url) {
+        const json_err = [{
+            "selected": false,
+            "value": undefined,
+            "label": "NON SONO PRESENTI CURRICULA"
+        }];
+        if(timetable_url === undefined) {
+            callback(json_err);
+        }
         var type = timetable_url.split('/')[3];
         var curricula_url = timetable_url + '/' + language[type] + '/@@available_curricula';
         // ex. https://corsi.unibo.it/laurea/clei/orario-lezioni/@@available_curricula
@@ -166,11 +92,6 @@ function getCurriculaGivenCourseUrl(unibo_url, callback) {
             })
             .catch(function (err) {
                 console.log(err);
-                json_err = [{
-                    "selected": false,
-                    "value": undefined,
-                    "label": "NON SONO PRESENTI CURRICULA"
-                }];
                 callback(json_err);
             });
     })
@@ -193,10 +114,10 @@ function getTimetable(unibo_url, year, curriculum, callback) {
                 });
                 lectures_form = '<button class="btn btn-secondary" id="select_or_deselect_all" onclick="return selectOrDeselectAll();">Deseleziona tutti</button>';
                 lectures_form += '<div class="container">';
-                lectures_form += '<form id="select_lectures" action="/get_calendar_url" method="post">';
+                lectures_form += '<form id="select_lectures" action="/get_calendar_url" method="post"><div class="row"><table>';
                 for (i = 0; i < inputs.length; i++)
-                    lectures_form += '<div class="row"><input type="checkbox" class="checkbox" name="lectures" value="' + inputs[i] + '" checked/><label>' + labels[i] + '</label></div></br>';
-                lectures_form += '<input type="hidden" name="timetable_url" value="' + timetable_url + '"/>';
+                    lectures_form += '<tr><th><input type="checkbox" class="checkbox" name="lectures" value="' + inputs[i] + '" checked/></th><th><label>' + labels[i] + '</label></th></tr>';
+                lectures_form += '</table></div><input type="hidden" name="timetable_url" value="' + timetable_url + '"/>';
                 lectures_form += '<input type="hidden" name="year" value="' + year + '"/>';
                 lectures_form += '<input type="hidden" name="curriculum" value="' + curriculum + '"/>';
                 lectures_form += '</div>';
@@ -260,7 +181,7 @@ function getICalendarEvents(timetable_url, year, curriculum, lectures, alert, ca
                 }
                 var url = 'Non è disponibile una aula virtuale';
                 if (!(l.teams === undefined) && !(l.teams === null)) {
-                    url = l.teams;
+                    url = encodeURIComponent(l.teams);
                 }
                 var prof = '-';
                 if (!(l.docente === undefined) && !(l.docente === null)) {
