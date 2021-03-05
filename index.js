7ef72f1cb26667a23345321661d226bcb89d708b
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-05 12:50:23 +0100

    Migrate old logs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var hbs = require('express-handlebars').create();
 var updater = require("./update_opendata.js");
+var csvMigrator = require("./migrate_csv.js");
 var sqlite3 = require('sqlite3').verbose();
 
 var db = new sqlite3.Database('./logs/data.db');
@@ -36,6 +37,7 @@ updater.checkForOpendataUpdates();
 db.run("CREATE TABLE IF NOT EXISTS enrollments (id TEXT, date INTEGER, type TEXT, course TEXT, year INTEGER, curriculum TEXT)");
 db.run("CREATE TABLE IF NOT EXISTS requested_lectures (enrollment_id TEXT, lecture_id TEXT)");
 db.run("CREATE TABLE IF NOT EXISTS hits (date INTEGER, enrollment_id TEXT, user_agent TEXT)");
+csvMigrator.migrate(db);
 db.close()
 
 //start server
