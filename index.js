d0228d99c041fa924bc2f25c9f220b559f26af4f
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-05 11:34:03 +0100

    Create DB tables, added SQLite as dependency, removed CSV lib

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var hbs = require('express-handlebars').create();
 var updater = require("./update_opendata.js");
+var sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database('logs/data.db');
 
 var app = express();
 
@@ -26,6 +29,14 @@ app.set('view engine', 'handlebars');
 
 // Update opendata on launch
 updater.checkForOpendataUpdates();
+
+/**
+ * Create DB tables and migrate CSV
+ */
+db.run("CREATE TABLE IF NOT EXISTS enrollments (uuid_value TEXT, date INTEGER, type TEXT, course TEXT, year INTEGER, curriculum TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS lectures (enrollment_id TEXT, lecture_id TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS hits (date INTEGER, enrollment_id TEXT, user_agent TEXT)");
+db.close()
 
 //start server
 app.listen(app.get('port'), '127.0.0.1', function () {
