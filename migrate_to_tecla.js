9a7774fefedec0e49b6536d2938f0a2360281467
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-10-06 12:57:36 +0200

    [WIP Migrator] Convert Curricula to Tecla

--- migrate_to_tecla.js
+++ migrate_to_tecla.js
@@ -1,9 +1,22 @@
+const fetch = require('node-fetch');
 var sqlite3 = require('sqlite3').verbose();
 
-function getTeclaIdFromTypeAndCourse(type, name, curriculum) {
-    let course_id = "8165";
+const language = {
+    "magistralecu": "orario-lezioni",
+    "magistrale": "orario-lezioni",
+    "laurea": "orario-lezioni",
+    "singlecycle": "timetable",
+    "1cycle": "timetable",
+    "2cycle": "timetable"
+}
+
+async function getTeclaIdFromTypeAndCourse(type, name, curriculum) {
+    let url = "https://corsi.unibo.it/" + type + "/" + name + "/" + language[type] + "/@@available_curricula";
+    let res = await fetch(url).then(x => x.json());
+    let course_id = res[0].label.split(" - ")[0];
     return "unibo.c." + course_id + ".p." + curriculum;
 }
+
 
 var db = new sqlite3.Database('./logs/data.db');
 db.close()
