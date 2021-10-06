1720c0f2648228280ea4b1c4b1ed808a243b4a95
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-10-06 21:18:52 +0200

    [Migration to Tecla] WIP

--- migrate_to_tecla.js
+++ migrate_to_tecla.js
@@ -14,6 +14,9 @@ async function getTeclaIdFromTypeAndCourse(type, name, curriculum) {
     let url = "https://corsi.unibo.it/" + type + "/" + name + "/" + language[type] + "/@@available_curricula";
     let res = await fetch(url).then(x => x.json());
     let course_id = res[0].label.split(" - ")[0];
+    if (isNaN(parseInt(course_id))) {
+        console.log(type, name, curriculum);
+    }
     return "unibo.c." + course_id + ".p." + curriculum;
 }
 
@@ -21,11 +24,39 @@ async function getTeachingIdsInCourse(type, name, year) {
     let url = "https://corsi.unibo.it/" + type + "/" + name + "/" + language[type] + "/@@orario_reale_json?anno=" + year;
     let apiResp = await fetch(url).then(x => x.json());
     let resp = {};
-    for(let i = 0; i < apiResp.lenght; i++) {
+    for (let i = 0; i < apiResp.lenght; i++) {
         resp[apiResp[i].extCode] = apiResp[i].cod_modulo;
     }
     return resp;
 }
 
-var db = new sqlite3.Database('./logs/data.db');
-db.close()
+function promisify(fn, query, params) {
+    console.log(fn)
+    return new Promise(function (res, rej) {
+        fn(query, params, function (err, rows) {
+            if (err === null) {
+                res(rows);
+            } else {
+                rej(err);
+            }
+        });
+    });
+}
+
+async function migrate() {
+    var db = new sqlite3.Database('./logs/data.db');
+    await promisify(db.run.bind(db), "BEGIN;");
+    await promisify(db.run.bind(db), "ALTER TABLE enrollments ADD COLUMN institution_id TEXT");
+    await promisify(db.run.bind(db), "UPDATE enrollments SET institution_id = \"unibo\"");
+    await promisify(db.run.bind(db), "ALTER TABLE enrollments ADD COLUMN curriculum_id TEXT");
+    let courses_old = await promisify(db.all.bind(db), "SELECT DISTINCT type, course, curriculum FROM enrollments;");
+    //console.log(courses_old);
+    for (let i = 0; i < courses_old.length; i++) {
+        let course_id = await getTeclaIdFromTypeAndCourse(courses_old[i].type, courses_old[i].course, courses_old[i].curriculum);
+        //console.log(course_id, courses_old[i]);
+    }
+    await promisify(db.run.bind(db), "COMMIT;");
+    db.close();
+}
+
+migrate();
