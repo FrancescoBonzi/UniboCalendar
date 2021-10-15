3cee205ec0877775fb4852d31ccaea70f1bb5327
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-10-15 08:36:09 +0200

    WIP new add to calendar layout

--- model.js
+++ model.js
@@ -196,7 +196,6 @@ function checkEnrollment(uuid_value, callback) {
 }
 
 function getICalendarEvents(id, ua, alert, callback) {
-    let alert = null
     checkEnrollment(id, function (isEnrolled) {
         if (!isEnrolled) {
             const start = new Date();
