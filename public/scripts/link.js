d797c8b4fe689c7f9e80583a536f4c90d629721e
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-10-15 09:15:54 +0200

    Correctly handle alerts in links

--- public/scripts/link.js
+++ public/scripts/link.js
@@ -1,6 +1,26 @@
-function copy() {
-    var copyText = document.getElementById("url");
-    copyText.select(); 
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-    document.execCommand("copy"); // Copy the text inside the field
+function toggle() {
+  let collapse = document.getElementById("alertCollapse");
+  if (collapse.classList.contains("show")) {
+    collapse.classList.remove("show");
+  } else {
+    collapse.classList.add("show");
   }
+}
+function updateLinks(input) {
+  let secs = (parseInt(input.value) | 0) * 60;
+  let qry = "&alert=" + secs.toString();
+  if (secs == 0) {
+    qry = "";
+  }
+  let links = document.getElementById("links").getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    let thisQuery = qry;
+    let startsWith = "&";
+    if (links[i].href.startsWith("http")) {
+      console.log(links[i].href);
+      thisQuery = encodeURIComponent(thisQuery);
+      startsWith = encodeURIComponent(startsWith);
+    }
+    links[i].href = links[i].href.split(startsWith)[0] + thisQuery;
+  }
+}
