3f321d29dd4b6108245544f5c1748d5f69d7c441
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-10-06 21:43:46 +0200

    Async controller

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ const model = require('./model.js');
 
 function bonk(req, res, next) {
     res.writeHead(302, {
-      location: "https://knowyourmeme.com/photos/1916585-bonk-cheems",
+        location: "https://knowyourmeme.com/photos/1916585-bonk-cheems",
     });
     res.end();
 }
@@ -18,22 +18,20 @@ function error500(err, req, res, next) {
     res.render('500');
 }
 
-function home_page(req, res, next) {
-    model.getAreas(function (areas) {
-        res.render('home', { 'page': 'home', 'areas': areas });
-    });
+async function home_page(req, res, next) {
+    let areas = await model.getAreas();
+    res.render('home', { 'page': 'home', 'areas': areas });
 }
 
-function course_page(req, res, next) {
+async function course_page(req, res, next) {
     const unibo_url = req.body.courses;
     const year = req.body.years;
     const curriculum = req.body.curricula;
-    model.getTimetable(unibo_url, year, curriculum, function (list) {
-        res.render('course', { 'page': 'course', 'list': list });
-    });
+    let list = await model.getTimetable(unibo_url, year, curriculum);
+    res.render('course', { 'page': 'course', 'list': list });
 }
 
-function get_calendar_url(req, res, next) {
+async function get_calendar_url(req, res, next) {
     const timetable_url = req.body.timetable_url;
     const type = timetable_url.split('/')[3];
     const course = timetable_url.split('/')[4];
@@ -45,46 +43,42 @@ function get_calendar_url(req, res, next) {
     } else if (typeof lectures === 'string') {
         lectures = [lectures];
     }
-    model.generateUrl(type, course, year, curriculum, lectures, function (url) {
-        res.render('link', { 'page': 'link', 'url': url });
-    });
+    let url = await model.generateUrl(type, course, year, curriculum, lectures);
+    res.render('link', { 'page': 'link', 'url': url });
 }
 
-function get_ical(req, res, next) {
+async function get_ical(req, res, next) {
     const id = req.query.id;
     let alert = req.query.alert === undefined ? null : parseInt(req.query.alert);
-    model.getICalendarEvents(id, req.get("User-Agent"), alert, function (unibo_cal) {
-        res.type("text/calendar");
-        res.set({
-            'Cache-Control': 'private',
-            'Cache-Control': 'max-age=86400',
-        });
-        res.send(unibo_cal);
+    let unibo_cal = await model.getICalendarEvents(id, req.get("User-Agent"), alert);
+    res.type("text/calendar");
+    res.set({
+        'Cache-Control': 'private',
+        'Cache-Control': 'max-age=86400',
     });
+    res.send(unibo_cal);
 }
 
-function get_courses_given_area(req, res, next) {
+async function get_courses_given_area(req, res, next) {
     var area = req.query.area;
-    model.getCoursesGivenArea(area, function (courses) {
-        res.type("application/json");
-        res.set({
-            'Cache-Control': 'public',
-            'Cache-Control': 'max-age=86400',
-        });
-        res.send(courses);
+    let courses = await model.getCoursesGivenArea(area);
+    res.type("application/json");
+    res.set({
+        'Cache-Control': 'public',
+        'Cache-Control': 'max-age=86400',
     });
+    res.send(courses);
 }
 
-function get_curricula_given_course(req, res, next) {
+async function get_curricula_given_course(req, res, next) {
     var url = req.body.url;
-    model.getCurriculaGivenCourseUrl(url, function (curricula) {
-        res.type("application/json");
-        res.set({
-            'Cache-Control': 'public',
-            'Cache-Control': 'max-age=86400',
-        });
-        res.send(JSON.stringify(curricula));
-    })
+    let curricula = await model.getCurriculaGivenCourseUrl(url);
+    res.type("application/json");
+    res.set({
+        'Cache-Control': 'public',
+        'Cache-Control': 'max-age=86400',
+    });
+    res.send(JSON.stringify(curricula));
 }
 
 exports.dispatcher = function (app) {
